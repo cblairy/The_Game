47bef8bdd2b053c2497252a14a374f25be147279
Corentin Blairy <corentinblairy1@gmail.com>
2022-07-13 12:21:33 +0000

    quelques correctifs minimes

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 include 'fonctions.php';
-echo "PRET POUR LA BASTON ?<br>";
 
 function combat($combattant1,$combattant2){
     while($combattant1->pv > 0 && $combattant2->pv > 0){
@@ -17,8 +16,10 @@ function combat($combattant1,$combattant2){
     // on sort de la boucle while (un perso est KO)
     if($combattant1->pv <= 0){
         console("$combattant1->name est KO.");
+        echo "$combattant1->name est KO.";
     } else {
         console("$combattant2->name est KO.");
+        echo "$combattant2->name est KO.";
     }
 }
 
@@ -32,7 +33,8 @@ function combat($combattant1,$combattant2){
     <title>Battle</title>
 </head>
 <body style=" background:url('images/sda.webp')">
-    <label for="champions">Choix du premier Champion</label>
+<h1 style="background:white; width:400px; border-radius: 5px; padding:10px">PRET POUR LA BASTON ?</h1>
+    <label for="champions" style="background:white; padding:5px; border-radius:5px">Choix du premier Champion</label>
     <select name="champions" id="champions">
         <option value="">--Choix1--</option>
         <option value="$homme">Homme</option>
@@ -40,17 +42,17 @@ function combat($combattant1,$combattant2){
         <option value="$orque">Orque</option>
     </select>
 <br>
-    <label for="champions2">Choix du second Champion</label>
+    <label for="champions2" style="background:white; padding:5px;border-radius:5px">Choix du second Champion</label>
     <select name="champions2" id="champions2">
         <option value="">--Choix 2--</option>
         <option value="$homme">Homme</option>
         <option value="$elfe">Elfe</option>
         <option value="$orque">Orque</option>
     </select>
-    <div style="background-color:white; width:350px; border-radius:5px" >
-    <br>le choix n'est pas encore disponible, oupsi<br><br>
+    <div style="background-color:white; width:310px; border-radius:5px; padding:10px" >
+    le choix n'est pas encore disponible, oupsi<br><br>
     <?php
-    combat($elfe,$orque);
+    combat($homme,$elfe);
     ?>
     </div>
 </body>
